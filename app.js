99bd3ab266185dec4e3caf8439d550669d664885
arfinaleusuf <arfinaleusuf@gmail.com>
2026-06-21 21:53:38 +0600

    parsefloat and parseint

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var test = 'hello world';
+// var test = 'hello world';
 
-console.log(test);
+// console.log(test);
+
+
+var num = "12.5";
+var num1 = 15;
+
+var result = parseFloat(num) + num1;
+
+console.log(result);
