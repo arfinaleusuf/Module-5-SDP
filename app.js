4b344913671b495ea160d7c69b804b48815f8c80
arfinaleusuf <arfinaleusuf@gmail.com>
2026-06-22 22:35:47 +0600

    for loop

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ var num1 = 15;
 var result = parseFloat(num) + num1;
 
 console.log(result);
+
+var status = 'rain';
+if(status = 'rain')
+{
+    console.log('baire jawa jabe na');
+}
+else{
+    console.log('baire jawa jabe na');
+}
