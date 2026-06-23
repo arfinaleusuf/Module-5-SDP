b68f50fec8ab3f2e299b61712f7c2227ae2efdef
arfinaleusuf <arfinaleusuf@gmail.com>
2026-06-23 21:27:02 +0600

    comment out unused variables and console logs

--- app.js
+++ app.js
@@ -3,18 +3,32 @@
 // console.log(test);
 
 
-var num = "12.5";
-var num1 = 15;
+// var num = "12.5";
+// var num1 = 15;
 
-var result = parseFloat(num) + num1;
+// var result = parseFloat(num) + num1;
 
-console.log(result);
+// console.log(result);
 
-var status = 'rain';
-if(status = 'rain')
-{
-    console.log('baire jawa jabe na');
+// var status = 'rain';
+// if(status = 'rain')
+// {
+//     console.log('baire jawa jabe na');
+// }
+// else{
+//     console.log('baire jawa jabe na');
+// }
+
+var person = {
+    hand: 2,
+    eye: 2,
+    hair: "dont know",
+    father: {
+        name: "jhon",
+        age: 40,
+
+    }
 }
-else{
-    console.log('baire jawa jabe na');
-}
+// console.log(person.hand);
+
+console.log(person.father.name);
